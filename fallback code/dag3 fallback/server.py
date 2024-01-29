b3f17a7916d90067c82b3cf025913cd3f5556e3c
Arne Madalijns <arnemadalijns@hotmail.com>
2024-01-29 10:32:03 +0100

    dag3 fallback

---
+++ fallback code/dag3 fallback/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    if data is not None:
+        latitude = data.get('latitude')
+        longitude = data.get('longitude')
+        alarm = data.get('Alarm')
+        print(f'Latitude: {latitude}, Longitude: {longitude}, Alarm: {alarm}')
+        with open('gps_data.txt', 'w') as f:
+            f.write(f'{latitude},{longitude}')
+        with open('button_state.txt', 'w') as f:
+            f.write(f'{alarm}')
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
