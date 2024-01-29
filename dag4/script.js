62e555ac2495d7149b5737e64b005a06290ffaac
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-01-29 11:43:31 +0100

    verboden zone bug fix na remove

--- dag4/script.js
+++ dag4/script.js
@@ -42,10 +42,13 @@ function processAndDisplayMarkers(data) {
     // Update the coordinates below the map
     document.getElementById('coordinates').textContent = 'Latitude: ' + lastCoord[0] + ', Longitude: ' + lastCoord[1];
 
-    // Check if the marker position crosses the border
-    if (circle && map.distance(lastCoord, circle.getLatLng()) > circle.getRadius()) {
-        // If marker crosses the border, trigger an alert
-        alert('Kind is in een verbodenzone!');
+     // Check if the circle exists before calculating the distance
+     if (circle) {
+        // Check if the marker position crosses the border
+        if (map.distance(lastCoord, circle.getLatLng()) > circle.getRadius()) {
+            // If marker crosses the border, trigger an alert
+            alert('Kind is in een verbodenzone!');
+        }
     }
 }
 
